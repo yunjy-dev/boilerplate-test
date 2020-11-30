6efa91b9a749bcc882aa5ca5096c8fc636d1536c
yunjy-tmax <junyeon_yun@tmax.co.kr>
2020-12-01 07:43:33 +0900

    The first commit

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name:       { type: String, maxlength:255},
+    lastname:   { type: String, maxlength:255},
+    email:      { type: String, maxlength:255, trim: true, unique: 1},
+    password:   { type: String, maxlength:255, minlength:5},
+    role:       { type: Number, default: 0},
+    image:      String,
+    token:      { type: String},
+    tokenExp:   { type: Number}
+})
+
+const userSchema = mongoose.model('user', userSchema);
+
+module.export = {user};
