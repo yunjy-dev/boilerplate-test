72fde1e33485556c43c20432daa9cd887aa7514d
yunjy-tmax <junyeon_yun@tmax.co.kr>
2020-12-01 11:37:11 +0900

    bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name:       { type: String, maxlength:255},
@@ -9,6 +11,29 @@ const userSchema = mongoose.Schema({
     image:      String,
     token:      { type: String},
     tokenExp:   { type: Number}
+})
+
+userSchema.pre('save', function(next){  //pre() from mongoose method
+
+    var user = this;
+
+    if(user.isModified('password')){
+    
+        //encrypt password by bcrypt
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err);
+
+            var myPlaintextPassword = user.password;
+            // console.log(myPlaintextPassword)
+            bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+                if(err) return next(err);
+                user.password = hash;
+                // Store hash in your password DB.
+                console.log(hash);
+                next();
+            });
+        });
+    }//end of if
 })
 
 const User = mongoose.model('User', userSchema);
